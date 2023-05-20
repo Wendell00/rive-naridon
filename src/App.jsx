f06fc7efdf4658db54cfda4543329eb2eb1d19da
wendell00 <wendellborges2003@gmail.com>
2023-05-19 22:10:20 -0300

    code fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,5 @@
-
 import './App.css'
 import { useRive } from "@rive-app/react-canvas";
-
-// function Rive() {
-
-//   // const state = useStateMachineInput(rive, "thunderGodMode", true);
-
-//   const { rive, RiveComponent } = useRive({
-//     src: "./naridonOniFanArt.riv",
-//     stateMachines: "State Machine 1",
-//     autoplay: true,
-//   });
-
-//   return (
-//     <RiveComponent
-//       style={{ height: "100vh"}}
-//     />
-//   );
-// }
 
 export default function Home() {
   const { rive, RiveComponent } = useRive({
